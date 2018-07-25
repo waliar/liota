d950895a8a27007c7da81a1d75e7434741b43ff5
waliar <41676153+waliar@users.noreply.github.com>
2018-07-25 15:56:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,10 +37,14 @@
 import os
 import pip
 import sys
+
 try: # for pip >= 10
     from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0
     from pip.req import parse_requirements
+    from pip.download import PipSession
+    
 from setuptools import setup, find_packages
 
 #
